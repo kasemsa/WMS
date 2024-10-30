0e71c962d29f7d2f2e639b7aabf747b09e3e90c8
kasemsa <kasemsa2000@gmail.com>
2024-10-31 01:40:45 +0300

    add Dbsets to DbContext

--- DataBase/WarehouseDbContext.cs
+++ DataBase/WarehouseDbContext.cs
@@ -1,6 +1,23 @@
-﻿namespace WarehouseManagementSystem.DataBase
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseManagementSystem.Models;
+
+namespace WarehouseManagementSystem.DataBase
 {
-    public class WarehouseDbContext
+    public class WarehouseDbContext : DbContext
     {
+        public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<UserPermission> UserPermissions { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Commissary> Commissaries { get; set; }
     }
 }
